a9384466b91151d9ac0d8899729781b022b3774c
sourabhchalke <sourabhchalke2157@gmail.com>
2024-08-11 15:47:46 +0530

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h2 className='m-4'>Bootstrap Links Added</h2>
+      <Navbar/>
     </div>
   );
 }
